17f70120bf17360fb191417b5c8b978968d2c442
JanAlexanderZak <janalexzak@gmail.com>
2020-11-24 09:08:12 +0100

    add: newyear

--- my_django_app/my_first_app/views.py
+++ my_django_app/my_first_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 
 def index(request):
@@ -10,4 +11,12 @@ def greet(request, name):
     return render(request, "my_first_app/greet.html", {
         # context is where variables have access to
         "name": name.capitalize()
+    })
+
+
+# Create your views here.
+def newyear(request):
+    now = datetime.datetime.now()
+    return render(request, "my_first_app/newyear.html", {
+        "newyear": now.month == 1 and now.day == 1,
     })
