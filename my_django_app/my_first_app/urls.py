8db42f30ed209b781925f4e96b9d2ae999ac21b7
JanAlexanderZak <janalexzak@gmail.com>
2020-11-24 09:08:34 +0100

    refactor: layout changes

--- my_django_app/my_first_app/urls.py
+++ my_django_app/my_first_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     # str:name is input from url, passes to greet1 function which takes as input: name
     path("<str:name>", views.greet, name="greet"),
+    path("newyear/", views.newyear, name="newyear"),
 
 ]
